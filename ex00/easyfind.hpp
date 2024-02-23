3235b90696618d1d572ca17a59978b36dca1e4b3
Shota Nemoto <snemoto@c1r14s5.42tokyo.jp>
2024-02-23 12:28:57 +0900

    0223-5

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -19,18 +19,20 @@ typename T::iterator easyfind(T & container, int value)
 	}
 }
 
-template <typename T>
-typename T::iterator const& easyfind(T const& container, int value)
-{
-	typename T::iterator it = std::find(container.begin(), container.end(), value);
-	if (it != container.end())
-	{
-		return it;
-	}
-	else
-	{
-		std::stringstream ss;
-		ss << value << " not fonund in container";
-		throw std::runtime_error(ss.str());
-	}
-}
+// cost_iterator は c++11 のためコメントアウト
+
+// template <typename T>
+// typename T::const_iterator const easyfind(T const& container, int value)
+// {
+// 	typename T::const_iterator const it = std::find(container.begin(), container.end(), value);
+// 	if (it != container.end())
+// 	{
+// 		return it;
+// 	}
+// 	else
+// 	{
+// 		std::stringstream ss;
+// 		ss << value << " not fonund in container";
+// 		throw std::runtime_error(ss.str());
+// 	}
+// }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,7 +5,7 @@
 int main(void)
 {
 	{
-		std::cout << "\n<<< Test for vector >>>" << std::endl;
+		std::cout << "\n<<< Test for non-const vector >>>" << std::endl;
 		std::vector<int> numbers;
 		std::vector<int>::iterator it;
 		int value = 20000;
@@ -20,6 +20,9 @@ int main(void)
 			--value;
 			it = easyfind(numbers, value);
 			std::cout << value << " found at position " << std::distance(numbers.begin(), it) << std::endl;
+			std::cout << "before value:" << *it << std::endl;
+			++(*it);
+			std::cout << "after value:" << *it << std::endl;
 		}
 		catch(const std::exception& e)
 		{
@@ -38,19 +41,20 @@ int main(void)
 		}			
 	}
 	{
-		std::cout << "\n<<< Test for list >>>" << std::endl;
+		std::cout << "\n<<< Test for non-const list >>>" << std::endl;
 		std::list<int> numbers;
 		std::list<int>::iterator it;
 		int value = 20000;
-		for (int i = value; 0 < i; --i)
+		for (int i = 0; i < value; ++i)
 		{
 			numbers.push_back(i);
 		}
+
 		std::cout << "\n>>> found the value" << std::endl;
 		try
 		{
-			it = easyfind(numbers, value);
-			std::cout << value << " found at position " << std::distance(numbers.begin(), it) << std::endl;
+			std::list<int>::iterator const& it = easyfind(numbers, value);
+			std::cout << *it << " found" << std::endl;
 		}
 		catch(const std::exception& e)
 		{
@@ -59,7 +63,7 @@ int main(void)
 		std::cout << "\n>>> not found the value" << std::endl;
 		try
 		{
-			it = easyfind(numbers, -1);
+			std::list<int>::iterator it = easyfind(numbers, -1);
 			std::cout << value << " found at position " << std::distance(numbers.begin(), it) << std::endl;
 		}
 		catch(const std::exception& e)
@@ -67,6 +71,32 @@ int main(void)
 			std::cerr << e.what() << std::endl;
 		}	
 	}
+	// {
+	// 	std::cout << "\n<<< Test for const list >>>" << std::endl;
+	// 	int array[] = {1, 2, 3, 4, 5};
+	// 	const std::list<int> numbers(array, array + sizeof(array) / sizeof(int));
+	// 	int value = 3;
+	// 	std::cout << "\n>>> found the value" << std::endl;
+	// 	try
+	// 	{
+	// 		std::list<int>::const_iterator const& it = easyfind(numbers, value);
+	// 		std::cout << *it << " found" << std::endl;
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << std::endl;
+	// 	}
+	// 	std::cout << "\n>>> not found the value" << std::endl;
+	// 	try
+	// 	{
+	// 		std::list<int>::const_iterator it = easyfind(numbers, -1);
+	// 		std::cout << value << " found at position " << std::distance(numbers.begin(), it) << std::endl;
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << e.what() << std::endl;
+	// 	}	
+	// }
 	std::cout << std::endl;
 	return 0;
 }
